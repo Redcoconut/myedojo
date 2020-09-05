94716f8d708141832450a5558f3a57d0387e2734
Ari Donnenfeld <ari@bournedigital.com.au>
2020-09-05 16:53:46 +1000

    Finished adding tabs and also a calendar page

--- google-calendar.js
+++ google-calendar.js
@@ -1,6 +1,31 @@
 /* * Solution from László L. L. on Stack Overflow:
     * https://stackoverflow.com/a/56496854/908059
     */
+function showAdvanced(test) {
+	console.log("About to show:")
+	console.log(test)
+	var adv = document.getElementById("js-gcal-event-advanced");
+	var yoga = document.getElementById("js-gcal-event-yoga");
+	var begi = document.getElementById("js-gcal-event-beginners");
+	yoga.style.display = "none";
+	begi.style.display = "none";
+	adv.style.display = "none";
+	document.getElementById("yoga_button").style.borderBottom = "none"
+	document.getElementById("advanced_button").style.borderBottom = "none"
+	document.getElementById("beginners_button").style.borderBottom = "none"
+	if (test == "Advanced") {
+		adv.style.display = "block";
+		document.getElementById("advanced_button").style.borderBottom = "1px solid grey"
+	} else if (test == "Yoga"){
+		yoga.style.display = "block";
+		document.getElementById("yoga_button").style.borderBottom = "1px solid grey"
+	} else if (test == "Beginners") {
+		begi.style.display = "block";
+		document.getElementById("beginners_button").style.borderBottom = "1px solid grey"
+
+	}
+
+}
 function printCalendar () {
 
   var calendarId = 'p4m8gr97fnskfiqslr4ihhsa88@group.calendar.google.com';
@@ -25,19 +50,25 @@ function printCalendar () {
       'timeZone': userTimeZone,
       'singleEvents': true,
       'timeMin': (new Date()).toISOString(), // gathers only events not happened yet
-      'maxResults': 5,
+      'maxResults': 30,
       'orderBy': 'startTime'
     });
   }).then(function (response) {
     //console.log(response); // TODO: Remove!
     if (response.result.items) {
-      var getNowPlayingDiv = document.getElementById('js-gcal-event-advanced'); // Make sure your HTML has This ID!
+      var getNowPlayingDivAdvanced = document.getElementById('js-gcal-event-advanced'); // Make sure your HTML has This ID!
+	  var getNowPlayingDivYoga = document.getElementById('js-gcal-event-yoga'); // Make sure your HTML has This ID!
+	  var getNowPlayingDivBeginners = document.getElementById('js-gcal-event-beginners'); // Make sure your HTML has This ID!
+
       // Create a table with events:
-      var calendarRows = ['<table id="eventable">'];
+      var calendarRowsAdvanced = ['<table id="eventable">'];
+	  var calendarRowsYoga = ['<table id="eventable">'];
+	  var calendarRowsBeginners = ['<table id="eventable">'];
+
       response.result.items.forEach(function (entry) {
         var eventDate = dayjs(entry.start.dateTime).format('LL'); // eg: March 26, 2020 6:00 PM
         var eventEndsAt = dayjs(entry.end.dateTime).format('LT'); // eg: 7:00 PM
-        calendarRows.push('' +
+		temp = '' +
 			'<td id="eventd"><div name="viewport" id="evendiv">' +
 		      	'<p id="eventitle" style="margin-left: 10px; margin-top: 25px; color: #C81010; max-width: 70%;">' + entry.summary + '</p>' +
 					'<div id="datediv">' +
@@ -49,11 +80,23 @@ function printCalendar () {
 				'<p id="evendate" style=" margin-left: 10px; color: grey; margin-top: 0;">' + dayjs(entry.start.dateTime).format('HH:mm') + ' - ' + dayjs(entry.end.dateTime).format('HH:mm') + '</p>' +
 		        '<p id="descrip">' + entry.description + '</p>' +
 		        '<a target="_blank" id="evenadd" href="' + entry.htmlLink + '">Add to you calendar<a>' +
-			'</div></td>');
+			'</div></td>'
+		if (entry.summary.includes("Advanced") && calendarRowsAdvanced.length < 6){
+			calendarRowsAdvanced.push(temp);
+		} else if (entry.summary.includes("Yoga") && calendarRowsYoga.length < 6) {
+			calendarRowsYoga.push(temp);
+		} else if (entry.summary.includes("Beginners") && calendarRowsBeginners.length < 6) {
+			calendarRowsBeginners.push(temp);
+		}
       });
+      calendarRowsAdvanced.push('</table>');
+	  calendarRowsYoga.push('</table>');
+	  calendarRowsBeginners.push('</table>');
 
-      calendarRows.push('</table>');
-      getNowPlayingDiv.innerHTML = calendarRows.join('');
+      getNowPlayingDivAdvanced.innerHTML = calendarRowsAdvanced.join('');
+	  getNowPlayingDivYoga.innerHTML = calendarRowsYoga.join('');
+	  getNowPlayingDivBeginners.innerHTML = calendarRowsBeginners.join('');
+
     }
   }, function (reason) {
     console.log('Error: ' + reason.result.error.message);
